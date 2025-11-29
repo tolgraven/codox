66a159383906212eba399300f9881863e3d6316e
Joen Tolgraven <joen@tolgraven.se>
2025-11-29 19:47:17 +0100

    lein: use private dep repo

--- lein-codox/project.clj
+++ lein-codox/project.clj
@@ -5,5 +5,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
+  :repositories [["private" {:url "https://tolgraven.hel1.your-objectstorage.com/m2/releases/"
+                             :no-auth true}]]
+  :deploy-repositories [["private-local" {:url "file:.deploy-m2"
+                                          :sign-releases false}]]
+
   :dependencies [[leinjacker "0.4.3"]
                  [org.clojure/core.unify "0.5.7"]])
